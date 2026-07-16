be81c83ddeb38766ca407f6b9157c3103de20f8d
ocampomarnie05-beep <ocampomarnie05@gmail.com>
2026-07-17 01:36:35 +0800

    Add files via upload

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+
+    <!-- Main Content -->
+    <v-main class="bg-grey-lighten-4">
+      <v-container class="py-5">
+        <slot />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup></script>
+
+<style></style>
